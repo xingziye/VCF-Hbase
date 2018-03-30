e0ae285087e6e05c946285a9cedc00e4d942174a
Ziye Xing <xingziye46@gmail.com>
2018-03-30 04:05:19 -0700

    Add createTable class

--- CreateTable.java
+++ CreateTable.java
@@ -1,33 +1,54 @@
 import java.io.IOException;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HConstants;
 import org.apache.hadoop.hbase.HTableDescriptor;
-import org.apache.hadoop.hbase.client.HBaseAdmin;
 import org.apache.hadoop.hbase.TableName;
-
-import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.client.Admin;
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.apache.hadoop.hbase.io.compress.Compression.Algorithm;
 
 public class CreateTable {
-      
-   public static void main(String[] args) throws IOException {
 
-      // Instantiating configuration class
-      Configuration con = HBaseConfiguration.create();
+    private Configuration config;
 
-      // Instantiating HbaseAdmin class
-      HBaseAdmin admin = new HBaseAdmin(con);
+    public CreateTable() {
+        config = HBaseConfiguration.create();
+    }
 
-      // Instantiating table descriptor class
-      HTableDescriptor tableDescriptor = new
-      HTableDescriptor(TableName.valueOf("emp"));
+    private static void createOrOverwrite(Admin admin, HTableDescriptor table) throws IOException {
+        if (admin.tableExists(table.getTableName())) {
+            admin.disableTable(table.getTableName());
+            admin.deleteTable(table.getTableName());
+        }
+        admin.createTable(table);
+    }
 
-      // Adding column families to table descriptor
-      tableDescriptor.addFamily(new HColumnDescriptor("personal"));
-      tableDescriptor.addFamily(new HColumnDescriptor("professional"));
+    public static void createSchemaTables(String tableName, String... columnFamilyNames) throws IOException {
+        try (Connection connection = ConnectionFactory.createConnection(config);
+             Admin admin = connection.getAdmin()) {
 
-      // Execute the table through admin
-      admin.createTable(tableDescriptor);
-      System.out.println(" Table created ");
-   }
+            HTableDescriptor table = new HTableDescriptor(TableName.valueOf(tableName));
+            for (String cf : columnFamilyNames) {
+                table.addFamily(new HColumnDescriptor(cf).setCompressionType(Algorithm.NONE));
+            }
+
+            System.out.print("Creating table. ");
+            createOrOverwrite(admin, table);
+            System.out.println(" Done.");
+        }
+    }
+
+    public static void main(String... args) throws IOException {
+
+        //Add any necessary configuration files (hbase-site.xml, core-site.xml)
+        config.addResource(new Path(System.getenv("HBASE_CONF_DIR"), "hbase-site.xml"));
+        config.addResource(new Path(System.getenv("HADOOP_CONF_DIR"), "core-site.xml"));
+        createSchemaTables(config);
+        modifySchema(config);
+    }
 }
---
+++ HbaseVCF.java
@@ -0,0 +1,7 @@
+import CreateTable;
+
+public class HbaseVCF {
+    public static void main(String[] args) {
+        CreateTable ct = new CreateTable();
+    }
+}
